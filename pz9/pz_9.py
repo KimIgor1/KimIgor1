6d61b45e2a5327d105c0a23619e1e8654fedfde1
KimIgor1 <astr7x@mail.ru>
2024-01-20 15:19:35 +0300

    task 3

--- pz9/pz_9.py
+++ pz9/pz_9.py
@@ -1,4 +1,4 @@
 #Выполнить сортировку словаря d = {'a': 1, 'b': 2, 'c': 3}
-d = {'c': 3, 'a': 1, 'b': 2}
-d = dict(sorted(d.items(), key=lambda x: x[0]))
+d = {'b': 2, 'c': 3, 'a': 1}
+d = dict(sorted(d.items()))
 print(d)
